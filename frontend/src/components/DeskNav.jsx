aeafb9587b53c4d86b174b886d0c49a8c7ad4413
Sonu Mondal <takeinfo2u@gmail.com>
2025-07-15 19:54:01 +0530

    added form validation while registration

--- frontend/src/components/DeskNav.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function DeskNav() {
-  return (
-    <div>DeskNav</div>
-  )
-}
-
-export default DeskNav
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -42,20 +42,24 @@ const Register = () => {
   };
 
   const handleSubmit = async (e) => {
-    setShowLoader(true)
     e.preventDefault();
+
+    if (!validateForm()) return;
+
+    setShowLoader(true);
     try {
       const res = await sendEmailVerificationCode(formData.email);
       setOtpCode(res);
       setShowModal(true);
-      toast.success("OTP successfully sent to your email")
-      setShowLoader(false)
+      toast.success("OTP successfully sent to your email");
     } catch {
-      setShowLoader(false)
       setError("Failed to send verification code");
-      toast.error("Something wnet wrong while sending otp, please try again")
+      toast.error("Something went wrong while sending OTP, please try again");
+    } finally {
+      setShowLoader(false);
     }
   };
+
 
 
   const onSuccess = async () => {
@@ -73,6 +77,52 @@ const Register = () => {
       toast.error("something went wrong while regustring, please try again")
     }
   }
+
+  const validateForm = () => {
+    const { name, email, password } = formData;
+
+    const trimmedName = name.trim();
+    const nameRegex = /^[A-Za-z]{3,} [A-Za-z]{3,}$/; // Only one space, both parts min 3 letters
+
+    if (!nameRegex.test(trimmedName)) {
+      setError("Name must be two words (min 3 characters each) with one space only");
+      return false;
+    }
+
+    if (trimmedName.length > 30) {
+      setError("Full name must be at most 30 characters including space");
+      return false;
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      setError("Please enter a valid email address");
+      return false;
+    }
+
+    if (email.length > 50) {
+      setError("Email must not exceed 50 characters");
+      return false;
+    }
+
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9])[\S]{8,}$/;
+
+    if (!passwordRegex.test(password)) {
+      setError(
+        "Password must be at least 8 characters, include uppercase, lowercase, number, and special character, and contain no spaces"
+      );
+      return false;
+    }
+
+    if(password.length>20){
+      setError("Password length should not exceed more than 20")
+      return false
+    }
+
+    setError("");
+    return true;
+  };
+
 
   return (
     <div className="register-wrapper">
